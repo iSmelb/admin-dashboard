98ee3e935c68883e12af8b67c1ea62fa36ceff5d
Ihor Prylespkyi <iprylepskyi@gbsfo.com>
2023-03-15 16:51:18 +0200

    created contacts, invoices, team pages

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import Contacts from "../scenes/contacts";
 import Dashboard from "../scenes/dashboard";
+import Invoices from "../scenes/invoices";
+import Team from "../scenes/team";
 
 const AppRoutes = () => {
   return(
     <Routes>
       <Route path="/" element={<Dashboard/>}/>
+      <Route path="/team" element={<Team/>}/>
+      <Route path="/contacts" element={<Contacts/>}/>
+      <Route path="/invoices" element={<Invoices/>}/>
+      
     </Routes>
   )
 }
