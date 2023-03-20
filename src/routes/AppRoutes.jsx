aa7444cd2c78e3456233c08e5b779b829d687dab
Ihor Prylespkyi <iprylepskyi@gbsfo.com>
2023-03-20 12:30:20 +0200

    added barchart

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router-dom";
+import Bar from "../scenes/bar";
 import Calendar from "../scenes/calendar";
 import Contacts from "../scenes/contacts";
 import Dashboard from "../scenes/dashboard";
+import FAQ from "../scenes/faq";
 import Form from "../scenes/form";
 import Invoices from "../scenes/invoices";
 import Team from "../scenes/team";
@@ -15,6 +17,8 @@ const AppRoutes = () => {
       <Route path="/invoices" element={<Invoices/>}/>
       <Route path="/form" element={<Form/>}/>
       <Route path="/calendar" element={<Calendar/>}/>
+      <Route path="/faq" element={<FAQ/>}/>
+      <Route path="/bar" element={<Bar/>}/>
     </Routes>
   )
 }
---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,80 @@
+import { Box, useTheme, Typography } from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import { ExpandMore } from "@mui/icons-material";
+import { tokens } from "../../theme";
+
+const FAQ = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page"/>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore/>}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Maxime quidem, sit tempore odio ea blanditiis temporibus qui possimus
+            inventore quis labore consequuntur dolor repudiandae voluptatum placeat veniam ex, sed nesciunt?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore/>}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Maxime quidem, sit tempore odio ea blanditiis temporibus qui possimus
+            inventore quis labore consequuntur dolor repudiandae voluptatum placeat veniam ex, sed nesciunt?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore/>}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Maxime quidem, sit tempore odio ea blanditiis temporibus qui possimus
+            inventore quis labore consequuntur dolor repudiandae voluptatum placeat veniam ex, sed nesciunt?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore/>}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Maxime quidem, sit tempore odio ea blanditiis temporibus qui possimus
+            inventore quis labore consequuntur dolor repudiandae voluptatum placeat veniam ex, sed nesciunt?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  )
+}
+
+export default FAQ
